4f8940be1714fcd032242f06e22f2a4b8e2ae3f5
doomguy <artiomat13100@gmail.com>
2023-03-14 09:50:18 +0200

    s

--- MulT.hpp
+++ MulT.hpp
@@ -3,39 +3,37 @@
 // Real-time Multi-threading monitoring/managment library.
 
 namespace MulT {
-	typedef void* (*CallBack)(void* Input);
-	typedef char GroupI;
+	class Monitor {
+		public:
+		typedef void* (*CallBack)(void* Input);
+		
+		int OptimalN;
+		
+		Monitor(CallBack CB, bool AutoLog);
+		~Monitor();
 
-	typedef short ThreadI;
+		// If you don't want outputs make it 0
+		void Open(int Num, void** Inputs, void** Outputs);
+		/*
+		Logs the CPU usage after last log and may perform some calculations.
+		If you didn't already log "last log" is just since Setup().
+		Returns weather or not OptimalN changed.
 
-	// Toptimal amount of threads possible, This is what makes MulT the GOAT.
-	// At first it's definately better than one thread, but MAY be worse than the real optimal, over time it improves.
-	extern int OptimalN;
+		!!!RULES OF THUMB!!!
+		TODO: Remove this and just fatalize program if all threads didn't stop
+		- Run Log after all threads stopped, you can do that with WaitFor(). Calculations assume all your measured threads died at the time of the log.
 
-	bool Setup();
-	// Wait for a specific group of threads to all return.
-	void WaitFor(GroupI G);
-	// Wait for a specific thread to return.
-	void WaitFor(ThreadI G);
-	/*
-	Logs the CPU usage after last log and may perform some calculations.
-	If you didn't already log "last log" is just since Setup().
-	Returns weather or not OptimalN changed.
+		- Do not Log in multiple places, a log is meant to happen once every tick/frame or whatever your metric is, every cycle.
+		
+		- Log either in the beginning of the cycle or it's end, explained below.
+		
+		- Log is not instant, it doesn't actually just log your CPU usage, it also may attempt to determine the next optimal number of threads possible, which could is a little for loop over the logs and a calculation. Watch out for the updates in OptimalN.
+		*/
+		void Log();
+		void Join();
 
-	!!!RULES OF THUMB!!!
-	TODO: Remove this and just fatalize program if all threads didn't stop
-	- Run Log after all threads stopped, you can do that with WaitFor(). Calculations assume all your measured threads died at the time of the log.
-
-	- Do not Log in multiple places, a log is meant to happen once every tick/frame or whatever your metric is, every cycle.
+		private:
+		bool AutoLog;
+	};
 	
-	- Log either in the beginning of the cycle or it's end, explained below.
-	
-	- Log is not instant, it doesn't actually just log your CPU usage, it also may attempt to determine the next optimal number of threads possible, which could is a little for loop over the logs and a calculation. Watch out for the updates in OptimalN.
-	*/
-	bool ComputeOptimalN();
-
-	GroupI AddGroup(CallBack CB);
-	// -1 if failed, can only fail if you exceed the optimal threads.
-	ThreadI OpenThread(GroupI G, void* Input);
-	void* GetThreadOutput(ThreadI Index);
 }
